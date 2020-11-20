a0cebb30ad2f43e79f8755e01ce2ba5029630a7c
백준하 <astoru@naver.com>
2020-11-20 12:37:20 +0900

    implements lambda handler

--- andang_extract_reddit_reaction/andang_extract_reddit_reaction/handler.py
+++ andang_extract_reddit_reaction/andang_extract_reddit_reaction/handler.py
@@ -2,4 +2,17 @@ from .src import main
 
 def lambda_handler(event, context):
     ##TODO event 객체로부터 필요 정보 추출하기.
-    main.main()
+    body = event['body'];
+    main.main(body['content_id'], body['source_name'], body['season_number'], body['episode_number'],
+              body['source'])
+
+    return {
+        'statusCode' : 200,
+        'body' :{
+            'content_id':body['content_id'],
+            'source_name':body['source_name'],
+            'statistics_type':body['statistics_type'],
+            'season_number':body['season_number'],
+            'episode_number':body['episode_number']
+        }
+    }
--- andang_extract_reddit_reaction/andang_extract_reddit_reaction/src/main.py
+++ andang_extract_reddit_reaction/andang_extract_reddit_reaction/src/main.py
@@ -2,7 +2,6 @@
 def main(content_id, source_name, season_number, episode_number, source_id):
     reddit_client = RedditComments(source_id)
     df = reddit_client.get_one_depth_comments()
-    # df.to_csv('episode1_toplevel_comments.csv', sep=',')
     s3_uploader = S3Uploader()
     s3_uploader.upload_dataframe(df, content_id, source_name, season_number, episode_number)
 
@@ -14,5 +13,6 @@ if __name__ == "__main__":
     load_dotenv(verbose = True)
     main("m123", "reddit_discussion", 1, 2, "323947")
 else:
+    ##TODO lambda upload시 src.redditApi.reddit_requester 형태가 되어야 함.
     from .redditApi.reddit_requester import RedditComments
     from .s3upload.s3_uploader import S3Uploader
