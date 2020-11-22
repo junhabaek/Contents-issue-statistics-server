08e44512cdfb13f2ff8feb351420b17585ef353d
백준하 <astoru@naver.com>
2020-11-22 13:36:34 +0900

    support season reaction extraction and support single reaction extraction

--- andang_extract_reddit_reaction/andang_extract_reddit_reaction/src/s3upload/s3_uploader.py
+++ andang_extract_reddit_reaction/andang_extract_reddit_reaction/src/s3upload/s3_uploader.py
@@ -12,8 +12,25 @@ class S3Uploader:
         csv_buf = StringIO()
         target_df.to_csv(csv_buf, header=True, index=False)
         csv_buf.seek(0)
+
+        formatted_path = ""
+        if season_number ==0:
+            formatted_path = self._get_single_formatted_path(content_id, source_name)
+        elif episode_number ==0:
+            formatted_path = self._get_season_formatted_path(content_id, source_name, season_number)
+        else:
+            formatted_path = self._get_formatted_path(content_id, source_name, season_number, episode_number)
+
+
         self._client.put_object(Bucket='big-andang', Body=csv_buf.getvalue(),
-                                Key=self._get_formatted_path(content_id, source_name, season_number, episode_number))
+                                Key=formatted_path)
 
     def _get_formatted_path(self, content_id, source_name, season_number, episode_number):
         return f'test/reactions/{content_id}/{source_name}/flattened_S{season_number}E{episode_number}.csv'
+
+    def _get_season_formatted_path(self, content_id, source_name, season_number):
+        return f'test/reactions/{content_id}/{source_name}/flattened_S{season_number}.csv'
+
+    def _get_single_formatted_path(self, content_id, source_name):
+        return f'test/reactions/{content_id}/{source_name}/flattened.csv'
+
